f4b0540f31bdadcdaaeedc24c14aab5dfd3ac1c8
Brahmaswaroop <brahmaswaroop12@gmail.com>
2024-12-27 16:55:15 +0530

    Implemented player animation

--- classes/player.py
+++ classes/player.py
@@ -1,17 +1,49 @@
-from .motion import Motion
 import pygame
+import os
+from .motion import Motion
 
 class Player(Motion):
-    def __init__(self, screen, ground):
+    def __init__(self, screen, ground, character: str):
         super().__init__(ground)
         self.speed = 7
+        self.sprite_index = 0
+        self.character = character
+        self.char_sprites = os.listdir(f"game_files/Character_skins/{character}")
+        print(self.char_sprites)
         self.screen = screen
         try:
-            self.surf = pygame.image.load("game_files/Character_skins/charSkin1.png").convert_alpha()
+            self.image = f"game_files/Character_skins/{character}/{self.char_sprites[-1]}"
+            self.surf = pygame.image.load(self.image).convert_alpha()
         except FileNotFoundError:
             self.surf = pygame.Surface((100, 100))
             self.surf.fill("black")
+        self.surf = pygame.transform.scale(self.surf, (150, 200))
         self.rect = self.surf.get_rect(midbottom=(screen.get_size()[0]/2, ground))
 
     def draw(self):
         self.screen.blit(self.surf, self.rect)
+
+    def animation(self, direction="stop"):
+        try:
+            if self.sprite_index >= 2:
+                self.sprite_index = 0
+            elif direction == "right":
+                self.image = f"game_files/Character_skins/{self.character}/{self.char_sprites[int(self.sprite_index)]}"
+                self.surf = pygame.image.load(self.image).convert_alpha()
+                self.sprite_index += 0.2
+            elif direction == "left":
+                self.image = f"game_files/Character_skins/{self.character}/{self.char_sprites[int(self.sprite_index)]}"
+                self.surf = pygame.image.load(self.image).convert_alpha()
+                self.surf = pygame.transform.flip(self.surf, True, False)
+                self.sprite_index += 0.2
+            elif direction == "stop":
+                self.image = f"game_files/Character_skins/{self.character}/{self.char_sprites[-1]}"
+                self.surf = pygame.image.load(self.image).convert_alpha()
+                self.sprite_index = 0
+            if self.rect.bottom < self.ground:
+                self.image = f"game_files/Character_skins/{self.character}/{self.char_sprites[1]}"
+                self.surf = pygame.image.load(self.image).convert_alpha()
+            self.surf = pygame.transform.scale(self.surf, (150, 200))
+            self.draw()
+        except Exception:
+            pass
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from classes.enemy import EnemyProcessing
 from classes.player import Player
 from classes.game_texts import disp_score, countdown_timer
-from classes.main_menu import build_main_menu
+from classes.start_menu import build_main_menu
 from classes.level_loader import load_level_data
 
 pygame.init()
@@ -15,10 +15,11 @@ ground = 520
 start_time = pygame.time.get_ticks()
 game_active = False
 game_start = False
+game_paused = False
 alpha_val = 0
 
 background_surf = pygame.image.load("game_files/Stage_backgrounds/back3.jpg").convert_alpha()
-player1 = Player(screen, ground)
+player1 = Player(screen, ground, "Character1")
 enemy_processor = EnemyProcessing(screen, ground)
 enemies = enemy_processor.enemy_generator(load_level_data("easy"))
 
@@ -32,15 +33,19 @@ while True:
     key_states = pygame.key.get_pressed()
     alpha_val, difficulty = build_main_menu(screen, game_active, alpha_val)
 
+    if game_start:
+        # countdown_timer(screen, background_surf, 3)
+        start_time = pygame.time.get_ticks()
+        game_start = False
+        continue
+
+    # elif game_paused:
+    #     game_paused_menu()
+
     # When game is running
     if game_active:
         # Drawing background and character
         screen.blit(background_surf, (0, 0))
-        if game_start:
-            countdown_timer(screen, background_surf, 3)
-            start_time = pygame.time.get_ticks()
-            game_start = False
-            continue
         player1.draw()
         player1.apply_gravity()
         enemy_processor.enemy_movement(enemies, difficulty)
@@ -50,10 +55,13 @@ while True:
     # Player controls when game is active
         if key_states[pygame.K_SPACE] or key_states[pygame.K_UP]:
             player1.jump()
+            player1.animation()
         elif key_states[pygame.K_LEFT]:
             player1.move_horizontal("left", border=True)
+            player1.animation(direction="left")
         elif key_states[pygame.K_RIGHT]:
             player1.move_horizontal("right", border=True)
+            player1.animation(direction="right")
 
     # Game over screen
     else:
